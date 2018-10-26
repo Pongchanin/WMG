408eee416ab394480f282e08508ab7bec3fdf7f0
twotthep <twotthep@me.com>
2018-10-26 09:48:00 +0700

    changes

---
+++ Shoot!!! (Top Down Form)/Assets/Script/Bullet/BasicBulletController.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicBulletController : MonoBehaviour
+{
+    Rigidbody2D rigid2D;
+    public float speed;
+    public Player1Controller player;
+    public int jumpTime = 0;
+
+
+    bool HitThewwall;
+
+    void setBallDirection()
+    {
+        if (player.PlayerFacingRight == true)
+        {
+            rigid2D.velocity = new Vector3(speed, 0, 0);
+        }
+        else
+        {
+            rigid2D.velocity = new Vector3(-speed, 0, 0);
+        }
+    }
+
+    void Start ()
+    {
+        rigid2D = gameObject.GetComponent<Rigidbody2D>();
+        GameObject Player = GameObject.Find("Player1");
+        player = Player.GetComponent<Player1Controller>();
+    }
+	
+
+	void Update ()
+    {
+        setBallDirection();
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player1")
+        {
+            Destroy(other.gameObject);
+        }
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Basic Bullet"))
+        {
+            jumpTime = 0;
+        }
+        else if (collision.collider.tag == "Player1")
+        {
+            Destroy(this.gameObject);
+        }
+        else if (collision.collider.tag == "Player")
+        {
+            Destroy(this.gameObject);
+        }
+        else if (collision.collider.tag == "ball")
+        {
+            Destroy(this.gameObject);
+        }
+        else if (collision.collider.tag == "bullet")
+        {
+            Destroy(this.gameObject);
+        }
+        else if (collision.collider.tag == "bullet3")
+        {
+            Destroy(this.gameObject);
+        }
+        else if (collision.collider.tag == "Player3")
+        {
+            Destroy(this.gameObject);
+        }
+        else if (collision.collider.tag == "Player4")
+        {
+            Destroy(this.gameObject);
+        }
+        else if (collision.collider.tag == "bullet4")
+        {
+            Destroy(this.gameObject);
+        }
+        else if (collision.collider.tag == "Player2")
+        {
+            Destroy(this.gameObject);
+        }
+
+    }
+}
--- Shoot!!! (Top Down Form)/Assets/Script/GravityTrapController.cs
+++ Shoot!!! (Top Down Form)/Assets/Script/GravityTrapController.cs
@@ -6,13 +6,23 @@ public class GravityTrapController : MonoBehaviour
 {
     public GameObject GravityTrap;
     float timeRemaining = 3f;
-    public GameObject player;
+    Rigidbody2D rigid2D;
+    public Player4Controller player;
+
+    //Player 3 Component
+    public Player3Controller player3;
+    public int reduceSpeed;
+    bool player3Contract;
 
     int playerSpeed;
 
 	void Start ()
     {
+        rigid2D = gameObject.GetComponent<Rigidbody2D>();
         GameObject Player = GameObject.Find("Player4");
+        player = Player.GetComponent<Player4Controller>();
+        player3 = GameObject.Find("Player3").GetComponent<Player3Controller>();
+        player3Contract = false;
     }
 
     private void Update()
@@ -21,20 +31,35 @@ public class GravityTrapController : MonoBehaviour
         if (timeRemaining <= 0)
         {
             Destroy(GravityTrap);
+            player3.moveSpeed += reduceSpeed;
         }
-        gameObject.transform.position = (new Vector3(GravityTrap.transform.position.x, GravityTrap.transform.position.y, 0));
+        gameObject.transform.position = (new Vector3(player.transform.position.x, player.transform.position.y, 0));
+        print("Player3 Speed: " + player3.moveSpeed);
+
     }
 
     void decreasePlayerSpeed()
     {
+        //Player 3 Speed -= DecreaseSpeedValue
 
+       player3.moveSpeed -= reduceSpeed;
+        print("Player3 Speed: " + player3.moveSpeed);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.collider.tag == "Player3")
+       
+        if (collision.collider.tag == "Player3" && player3Contract == false)
         {
             decreasePlayerSpeed();
+            player3Contract = true;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Player3" && player3Contract == true)
+        {
+            player3Contract = false;
         }
     }
 }
