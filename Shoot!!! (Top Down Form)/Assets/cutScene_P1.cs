f8813426a00ae8e24151b41e8f4c147cf1bbd1cc
Pongchanin Sakulrattanasak <ps527@sci.tu.ac.th>
2018-10-26 15:07:53 +0700

    new

---
+++ Shoot!!! (Top Down Form)/Assets/cutScene_P1.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cutScene_P1 : MonoBehaviour {
+   public GameObject cutSceneP1;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.E))
+        {
+            cutSceneP1.SetActive(true);
+            Time.timeScale = 0;
+        }
+        else
+        {
+            cutSceneP1.SetActive(false);
+           // Time.timeScale = 1;
+        }
+	}
+}
